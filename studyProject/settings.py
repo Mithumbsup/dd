92c9a2f45ee6624f8c2672d0c32d4b90ab41b01f
Mithumbsup <sosgik96@naver.com>
2019-03-07 23:27:52 +0900

    remove staticfiles dirs line

--- studyProject/settings.py
+++ studyProject/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'portfolio', 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'portfolio', 'static')
+# ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
